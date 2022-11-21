98e4ce968331686055e2969f83927c60a1b9f171
Ehsanba23 <ebashiri33@gmail.com>
2022-11-21 13:41:57 +0330

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class test
+{
+    public static void Main()
+    {
+        string a = Console.ReadLine();
+        int x = Convert.ToInt32(a);
+        double L = P1(x);
+        double K = P2(x);
+        Console.WriteLine(K);
+        Console.WriteLine(L);
+
+    }
+
+    private static double P1(double f)
+    {
+        double v = Math.Pow(f, 2);
+        return v;
+    }
+    private static double P2(double g)
+    {
+        double h = Math.Pow(g, 3);
+        return h;
+    }
+}
